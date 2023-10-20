3df3ff374a1ba94b70ede14ab0f7d7084202b243
thesatyarth <103782897+thesatyarth@users.noreply.github.com>
2023-10-20 22:28:59 +0530

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.tempconverter
+import android.app.AlertDialog
+import android.app.AlertDialog.Builder
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.ayushsb24.tempconverter.databinding.ActivityMainBinding
+import java.text.DecimalFormat
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var selectedUnit: String
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val df = DecimalFormat("#.##")//Decimal formatter
+        selectedUnit = "Fahrenheit"
+        binding.selectType.setOnClickListener() {
+            showAlertDialog()
+        }
+        binding.editInput.addTextChangedListener() {
+            val resultText: String
+            var inputVal = binding.editInput.text.toString()
+            if (inputVal.isNotEmpty()) {
+                var doubleInput = inputVal.toDouble()
+                if(selectedUnit == "Fahrenheit"){
+                    resultText = df.format((doubleInput - 32) * 5 / 9)
+                    binding.textResultType.text = "Celsius"
+                }else{
+                    //(0°C × 9/5) + 32
+                    resultText = df.format((doubleInput *9/5)+32)
+                    binding.textResultType.text = "Fahrenheit"
+                }
+                binding.textResult.text = resultText
+            }
+        }
+    }
+    private fun showAlertDialog() {
+        val alertDialog: Builder = Builder(this@MainActivity)
+        alertDialog.setTitle("Select Unit") //Setting title for alertBox
+        val items = arrayOf("Fahrenheit", "Celsius")
+        val checkedItem = -1
+        alertDialog.setSingleChoiceItems(items, checkedItem,
+            DialogInterface.OnClickListener { dialog, which ->
+                selectedUnit = items[which]
+                binding.textType.setText(items[which])
+            })
+        alertDialog.setPositiveButton(
+            android.R.string.ok,
+            DialogInterface.OnClickListener { dialog, which ->
+                dialog.dismiss()
+            })
+        val alert: AlertDialog = alertDialog.create()
+        alert.setCanceledOnTouchOutside(false)
+        alert.show()
+    }
+}
